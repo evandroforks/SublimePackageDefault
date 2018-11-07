ac2d45f258e624940fef7fe3ae1f9f98297b0bcf
evandrocoan <evandrocoan@hotmail.com>
2018-11-07 11:01:51 -0200

    Fixed platform_edit_settings.py now loading windows and linux
    settings

--- platform_edit_settings.py
+++ platform_edit_settings.py
@@ -10,11 +10,7 @@ class PlatformEditSettings(sublime_plugin.WindowCommand):
         platform        = sublime.platform()
         packages_folder = sublime.packages_path()
 
-        if platform in ("linux", "windows"):
-            setting_file = os.path.join( packages_folder, "Default", "Default.sublime-keymap" )
-
-        else:
-            setting_file = os.path.join( packages_folder, "Default", "Default (%s).sublime-keymap" % platform )
+        setting_file = os.path.join( packages_folder, "Default", "Default (%s).sublime-keymap" % platform )
 
         self.window.run_command( "edit_settings",
             {
