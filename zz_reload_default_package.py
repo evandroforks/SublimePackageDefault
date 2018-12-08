40c94154e9125a6e6c3d81f2c9819124b2912724
evandrocoan <evandrocoan@hotmail.com>
2018-12-08 01:37:56 -0200

    Fixed the zz_reload_default_package.py creating 0_packages_loader
    on the Loose Packages directory.

--- zz_reload_default_package.py
+++ zz_reload_default_package.py
@@ -1,15 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import sublime
+
 import os
 import sys
 import shutil
+import json
+import time
 import filecmp
 
+import hashlib
 import textwrap
+import traceback
 import threading
 
+from collections import OrderedDict
+
+skip_packing = False
+
+try:
+    from package_control.package_manager import PackageManager
+    from package_control.package_disabler_iterator import IgnoredPackagesBugFixer
+
+except ImportError:
+    skip_packing = True
+
 PACKAGE_ROOT_DIRECTORY = os.path.dirname( os.path.dirname( os.path.realpath( __file__ ) ) )
+
+SETTINGS_PACKAGE_NAME = '0_settings_loader'
+SETTINGS_PACKAGE_DIRECTORY = os.path.join( PACKAGE_ROOT_DIRECTORY, SETTINGS_PACKAGE_NAME )
+
+README_PACKAGE_FILE = os.path.join( SETTINGS_PACKAGE_DIRECTORY, 'README.md' )
+DEFAULT_PACKAGE_DIRECTORY = os.path.join( PACKAGE_ROOT_DIRECTORY, 'Default' )
+
+g_settings_files = \
+[
+    "Default (Linux).sublime-mousemap.hide",
+    "Default (Linux).sublime-keymap.hide",
+    "Default (OSX).sublime-keymap.hide",
+    "Default (OSX).sublime-mousemap.hide",
+    "Default (Windows).sublime-mousemap.hide",
+    "Default (Windows).sublime-keymap.hide",
+    "Preferences (Linux).sublime-settings.hide",
+    "Preferences (OSX).sublime-settings.hide",
+    "Preferences (Windows).sublime-settings.hide",
+    "Preferences.sublime-settings.hide",
+]
 
 
 def compare_text_with_file(input_text, file):
@@ -73,6 +110,48 @@ def create_reloader():
             output_file.write( reloader_code )
 
 
+def compute_file_hash(file_path):
+    """ https://stackoverflow.com/questions/22058048/hashing-a-file-in-python """
+    # BUF_SIZE is totally arbitrary, change for your app!
+    # lets read stuff in 64kb chunks!
+    BUF_SIZE = 65536
+    sha256 = hashlib.sha256()
+
+    with open( file_path, 'rb' ) as file:
+
+        while True:
+            data = file.read( BUF_SIZE )
+            if not data: break
+            sha256.update( data )
+
+    return sha256.hexdigest()
+
+
+def check_settings_changes():
+    has_changed_hashes = False
+    hashes_cache_path = os.path.join( sublime.cache_path(), "zz_reload_default_package.json" )
+
+    clean_file_hashes = {}
+    loaded_file_hashes = load_data_file( hashes_cache_path )
+
+    for file_name in g_settings_files:
+        file_path = os.path.join( DEFAULT_PACKAGE_DIRECTORY, file_name )
+        current_hash = compute_file_hash( file_path )
+
+        if file_name in loaded_file_hashes:
+
+            if current_hash != loaded_file_hashes[file_name]:
+                has_changed_hashes = True
+                print( "[zz_reload_default_package.py] Hash change for setting file: %s" % file_path )
+
+        clean_file_hashes[file_name] = current_hash
+
+    if clean_file_hashes != loaded_file_hashes:
+        write_data_file( hashes_cache_path, clean_file_hashes )
+
+    return has_changed_hashes
+
+
 def create_settings_loader():
     notice_code = \
     """
@@ -91,12 +170,6 @@ def create_settings_loader():
     """
     notice_code = textwrap.dedent(notice_code).lstrip()
 
-    SETTINGS_PACKAGE_NAME = '0_settings_loader'
-    SETTINGS_PACKAGE_DIRECTORY = os.path.join( PACKAGE_ROOT_DIRECTORY, SETTINGS_PACKAGE_NAME )
-
-    README_PACKAGE_FILE = os.path.join( SETTINGS_PACKAGE_DIRECTORY, 'README.md' )
-    DEFAULT_PACKAGE_DIRECTORY = os.path.join( PACKAGE_ROOT_DIRECTORY, 'Default' )
-
     if not os.path.exists( SETTINGS_PACKAGE_DIRECTORY ):
         os.makedirs( SETTINGS_PACKAGE_DIRECTORY )
 
@@ -106,21 +179,7 @@ def create_settings_loader():
             print( "[zz_reload_default_package.py] Updating the documentation file: %s" % README_PACKAGE_FILE )
             output_file.write( notice_code )
 
-    settings_files = \
-    [
-        "Default (Linux).sublime-mousemap.hide",
-        "Default (Linux).sublime-keymap.hide",
-        "Default (OSX).sublime-keymap.hide",
-        "Default (OSX).sublime-mousemap.hide",
-        "Default (Windows).sublime-mousemap.hide",
-        "Default (Windows).sublime-keymap.hide",
-        "Preferences (Linux).sublime-settings.hide",
-        "Preferences (OSX).sublime-settings.hide",
-        "Preferences (Windows).sublime-settings.hide",
-        "Preferences.sublime-settings.hide",
-    ]
-
-    for file in settings_files:
+    for file in g_settings_files:
         full_path = os.path.join( DEFAULT_PACKAGE_DIRECTORY, file )
         full_destine_path = os.path.join( SETTINGS_PACKAGE_DIRECTORY, file.rstrip('.hide') )
 
@@ -137,11 +196,75 @@ def create_settings_loader():
         print( "[zz_reload_default_package.py] Updating the setting file: %s" % full_path )
         shutil.copyfile( full_path, full_destine_path )
 
+    print( "" )
+    if skip_packing:
+        return
+
+    manager = PackageManager()
+    settings_package_file = os.path.join( sublime.installed_packages_path(), "%s.sublime-package" % SETTINGS_PACKAGE_NAME )
+    settings_package_cache = os.path.join( sublime.cache_path(), "%s.sublime-package" % SETTINGS_PACKAGE_NAME )
+
+    if manager.create_package( SETTINGS_PACKAGE_NAME, sublime.cache_path() ):
+        shutil.rmtree( SETTINGS_PACKAGE_DIRECTORY )
+        print( "[zz_reload_default_package.py] Creating the package file %s" % settings_package_cache )
+
+    else:
+        print( "[zz_reload_default_package.py] Error: Could not create the package file %s" % settings_package_cache )
+
+    for package in IgnoredPackagesBugFixer( [SETTINGS_PACKAGE_NAME], "upgrade" ):
+        shutil.move( settings_package_cache, settings_package_file )
+
+    print( "[zz_reload_default_package.py] Finished installing the package file %s" % settings_package_file )
+
 
 def run_operations():
     create_reloader()
-    create_settings_loader()
+
+    if check_settings_changes():
+        create_settings_loader()
 
 
-threading.Thread(target=run_operations).start()
+def plugin_loaded():
+    threading.Thread(target=run_operations).start()
+
+
+def write_data_file(file_path, dictionary_data):
+
+    with open( file_path, 'w', newline='\n', encoding='utf-8' ) as output_file:
+        json.dump( dictionary_data, output_file, indent='\t', separators=(',', ': ') )
+        output_file.write('\n')
+
+
+def load_data_file(file_path, wait_on_error=True):
+    """
+        Attempt to read the file some times when there is a value error. This could happen when the
+        file is currently being written by Sublime Text.
+    """
+    dictionary_data = {}
+
+    if os.path.exists( file_path ):
+        maximum_attempts = 3
+
+        while maximum_attempts > 0:
+
+            try:
+                with open( file_path, 'r', encoding='utf-8' ) as studio_channel_data:
+                    return json.load( studio_channel_data, object_pairs_hook=OrderedDict )
+
+            except ValueError as error:
+                print( "[zz_reload_default_package.py] Error, maximum_attempts %d, load_data_file: %s, %s" % (
+                        maximum_attempts, file_path, error ) )
+                maximum_attempts -= 1
+
+                if wait_on_error:
+                    time.sleep( 0.1 )
+
+        if maximum_attempts < 1:
+            print( "[zz_reload_default_package.py] Error: maximum_attempts exausted on file_path: %s" % file_path )
+
+    else:
+        print( "[zz_reload_default_package.py] Error on load_data_file(1), the file '%s' does not exists! \n%s\n" % (
+                file_path, "".join( traceback.format_stack() ) ) )
+
+    return dictionary_data
 
