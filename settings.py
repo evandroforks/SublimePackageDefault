561635e6742697d36d1be4755afa4d30cf9321b7
evandrocoan <evandrocoan@hotmail.com>
2019-03-17 00:59:18 -0300

    Created the command toggle_settings on settings.py and the
    keybind Ctrl+K, Ctrl+N to mimic a gutter hide because the
    Sublime Text gutter cannot be hidden without breaking the
    line_highlight setting.
    
    See the issue:
    highlight_line doesn't work unless gutter is true
    https://github.com/SublimeTextIssues/Core/issues 586

--- settings.py
+++ settings.py
@@ -307,3 +307,66 @@ class OpenFileSettingsCommand(sublime_plugin.WindowCommand):
 
     def is_enabled(self):
         return self.window.active_view() is not None
+
+
+per_window_settings = {}
+capture_new_window_from = None
+
+def set_settings(window_views, active_view_settings):
+
+    for view in window_views:
+
+        for setting in active_view_settings:
+            view.settings().set(setting, active_view_settings[setting])
+
+
+class ToggleSettingsCommand(sublime_plugin.WindowCommand):
+    """
+    Given several settings, toggle their values.
+    window.run_command("toggle_settings", {"settings": ["fold_buttons", "line_numbers"]})
+    """
+
+    def run(self, settings):
+        if not isinstance(settings, list): settings = [settings]
+        window_id = self.window.id()
+        active_view_settings = {}
+
+        per_window_settings[window_id] = active_view_settings
+        active_view = self.window.active_view()
+
+        for setting in settings:
+            active_view_settings[setting] = not active_view.settings().get(setting, False)
+
+        set_settings(self.window.views(), active_view_settings)
+
+
+class ToggleSettingsCommandListener(sublime_plugin.EventListener):
+
+    def on_new_async(self, view):
+        global capture_new_window_from
+        window = sublime.active_window()
+        window_id = window.id()
+
+        if capture_new_window_from is not None:
+            active_view_settings = capture_new_window_from
+            capture_new_window_from = None
+            per_window_settings[window_id] = active_view_settings
+
+            window_views = [window.active_view()]
+            window_views.extend(window.views())
+            set_settings(window_views, active_view_settings)
+
+        elif window_id in per_window_settings:
+            active_view_settings = per_window_settings[window_id]
+            set_settings(window.views(), active_view_settings)
+
+    def on_window_command(self, window, command_name, args):
+
+        if command_name == "new_window":
+            window_id = window.id()
+
+            if window_id in per_window_settings:
+                active_view_settings = per_window_settings[window_id]
+
+                global capture_new_window_from
+                capture_new_window_from = active_view_settings
