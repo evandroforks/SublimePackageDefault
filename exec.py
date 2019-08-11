88b2b1831ab48dac60cd4f15b5ef70ba1fe71f74
evandrocoan <evandrocoan@hotmail.com>
2019-08-11 20:37:38 -0300

    Created the package FixedToggleFindPanel for the exec.py hacks
    https://github.com/evandrocoan/FixedToggleFindPanel

--- exec.py
+++ exec.py
@@ -20,9 +20,15 @@ g_last_scroll_positions = {}
 g_last_click_time = time.time()
 g_last_click_buttons = None
 
-g_widget_view = None
-g_is_panel_focused = False
-g_is_widget_focused = False
+try:
+    from FixedToggleFindPanel.fixed_toggle_find_panel import is_panel_focused
+
+except ImportError as error:
+    print('Default.exec Error: Could not import the FixedToggleFindPanel package!', error)
+
+    def is_panel_focused():
+        return True
+
 
 # https://forum.sublimetext.com/t/why-do-i-call-show-panel-with-output-exec-but-need-to-call-window-find-output-panel-exec-instead-of-window-find-output-panel-output-exec/45739
 def get_panel_name(panel_name):
@@ -45,7 +51,7 @@ class ExecOutputFocusCancelBuildCommand(sublime_plugin.WindowCommand):
         if active_panel:
             panel_view = get_panel_view( window, active_panel )
 
-            if g_is_panel_focused:
+            if is_panel_focused():
                 user_notice = "Cancelling the build for '%s'..." % active_panel
                 print( str(datetime.datetime.now())[:-4], user_notice )
 
@@ -59,30 +65,7 @@ class ExecOutputFocusCancelBuildCommand(sublime_plugin.WindowCommand):
             self.window.run_command('show_panel', args={'panel': 'output.exec'})
 
 
-class HackListener(sublime_plugin.EventListener):
-
-    def on_new(self, view):
-        # print('on_new', view.buffer_id())
-
-        if not view.file_name():
-            view.settings().set('is_new_file', True)
-
-    def on_activated(self, view):
-        global g_widget_view
-        global g_is_panel_focused
-        global g_is_widget_focused
-
-        g_widget_view = view
-        active_window = sublime.active_window()
-        view_has_name = not not view.file_name() or view.settings().get('is_new_file')
-
-        g_is_widget_focused = view.settings().get('is_widget')
-        active_panel = active_window.active_panel()
-        g_is_panel_focused = active_panel and not view_has_name and not g_is_widget_focused
-
-        # print( "view_has_name:", view_has_name )
-        # print( "g_is_widget_focused:", g_is_widget_focused )
-        # print( "g_is_panel_focused:", g_is_panel_focused )
+class FullRegexListener(sublime_plugin.EventListener):
 
     def replaceby(self, value, replacements):
         # print('replacements', replacements)
@@ -169,46 +152,6 @@ class HackListener(sublime_plugin.EventListener):
 
             g_last_click_time = new_click
             g_last_click_buttons = clicks_buttons
-
-
-# https://github.com/SublimeTextIssues/Core/issues/2914
-class FixedToggleFindPanelCommand(sublime_plugin.WindowCommand):
-
-    def run(self, command, args={}):
-        window = self.window
-        active_group = window.active_group()
-        active_panel = window.active_panel()
-
-        focused_widget = g_widget_view
-        panel_had_focus = g_is_panel_focused
-        widget_had_focus = g_is_widget_focused
-
-        # print('panel_had_focus', panel_had_focus)
-        # print('widget_had_focus', widget_had_focus)
-        self.window.run_command( "show_panel", { "panel": "find", "reverse": False } )
-        self.window.run_command( command, args )
-
-        # print('FixedToggleFindPanel running', command, args)
-        sublime.status_message( "Successfully toggled the setting '%s'" % command )
-
-        if active_panel:
-            self.window.run_command( "show_panel", { "panel": active_panel } )
-
-            if panel_had_focus:
-                panel_view = get_panel_view( window, active_panel )
-                self.window.focus_view( panel_view )
-
-            elif widget_had_focus and focused_widget:
-                self.window.focus_view( focused_widget )
-
-            else:
-                self.window.focus_group( active_group )
-
-        else:
-            self.window.run_command( "hide_panel" )
-
-            if widget_had_focus and focused_widget:
-                self.window.focus_view( focused_widget )
 
 
 class ProcessListener(object):
